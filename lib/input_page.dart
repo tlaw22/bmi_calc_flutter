71732b42ef739aa0d4ed2cf06ea2b89066e8705b
tlaw22 <108081811+tlaw22@users.noreply.github.com>
2023-05-05 22:01:22 -0400

    errors

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-
 const bottomContainerHeight = 45.0;
 const activeCardColour = Color(0xFF2E2D2E);
 const bottomContainerColour = Color(0xFFEB1555);
-
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
@@ -21,70 +18,53 @@ class _InputPageState extends State<InputPage> {
           children: <Widget>[
             Expanded(
                 child: Row(
-                  children: const [
-                  Expanded(
+              children: const [
+                Expanded(
                     child: ReusableCard(
                       Colour: activeCardColour,
-                      cardChild: Column(
-                        children: <Widget>[
-                        Icon(
-                        FontAwesomeIcons.mars,
-                        size: 80,
-
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      Text('MALE', style: TextStyle(
-                        fontSize: 18.0, color: Color(0xFF8D8E98),),
-                      ),)
-                    ],
-                  ),
                 )),
+                Expanded(
+                    child: ReusableCard(
+                      Colour: activeCardColour,
+                )),
+
+              ],
+
+            )),
+            const Expanded(
+              child: ReusableCard(
+                Colour: Colors.amber,
+              ),
+            ),
             Expanded(
-                child: ReusableCard(
-                  Colour: activeCardColour,
-                )),
-
+                child: Row(
+              children: const [
+                Expanded(
+                  child: ReusableCard(
+                    Colour: activeCardColour,
+                  ),
+                ),
+                Expanded(
+                  child: ReusableCard(
+                    Colour: activeCardColour,
+                  ),
+                ),
+              ],
+            ),
+            ),
+            Container(
+              color: bottomContainerColour,
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            ),
           ],
-
-        )),
-    const Expanded(
-    child: ReusableCard(
-    Colour: Colors.amber,
-    ),
-    ),
-    Expanded(
-    child: Row(
-    children: const [
-    Expanded(
-    child: ReusableCard(
-    Colour: activeCardColour,
-    ),
-    ),
-    Expanded(
-    child: ReusableCard(
-    Colour: activeCardColour,
-    ),
-    ),
-    ],
-    ),
-    ),
-    Container(
-    color: bottomContainerColour,
-    margin: const EdgeInsets.only(top: 10.0),
-    width: double.infinity,
-    height: bottomContainerHeight,
-    ),
-    ]
-    ,
-    )
-    );
+        ));
   }
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({super.key, this.Colour, required this.cardChild});
+  const ReusableCard({super.key, this.Colour, this.cardChild});
 
   final Colour; // color filled in
   final Widget cardChild;
